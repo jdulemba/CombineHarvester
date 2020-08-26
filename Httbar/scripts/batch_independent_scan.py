5d31cba1bfd6140e660a48b97e7ac3b6d38cff78
jdulemba <josdrw9@gmail.com>
2020-08-26 17:28:42 +0200

    Job submission via condor now requires manually setting PATH in batch_job.sh

--- Httbar/scripts/batch_independent_scan.py
+++ Httbar/scripts/batch_independent_scan.py
@@ -5,6 +5,8 @@ import pickle
 from argparse import ArgumentParser
 from distutils import spawn
 from numpy import arange
+from pdb import set_trace
+import subprocess
 
 parser = ArgumentParser()
 parser.add_argument('outdir')
@@ -25,32 +27,55 @@ if not os.path.isdir(args.outdir):
 
     # set environment variables
 jobid = os.environ['jobid']
+proj_dir = os.environ['PROJECT_DIR']
+pypath = '/'.join(subprocess.Popen("which python", stdout=subprocess.PIPE, shell=True).stdout.read().split('\n')[0].split('/')[:-1])
 
 do_not_remove = set(args.doNotRemove.split(',')) if args.doNotRemove else set()
 
 masses = [str(j) for j in arange(*[int(i) for i in args.masses.split(':')])] \
 	if ':' in args.masses else args.masses.split(',')
 
+    ## write batch_job.sh
+with open('%s/batch_job.sh' % args.outdir, 'w') as batch_job:
+    batch_job.write("""#!/bin/bash
+
+export PATH={PYPATH}:$PATH
+echo  "which python"
+which python
+
+echo "python path"
+echo $PYTHONPATH
+
+echo "SCRAM_ARCH: " $SCRAM_ARCH
+
+EXE="${{@}}"
+echo $EXE
+
+echo "Executing: python {exe}" $EXE
+python {exe} $EXE
+""".format(PYPATH=pypath, exe=spawn.find_executable('single_point_limit.py')) )
+
+
+    ## write condor.jdl file
 with open('%s/condor.jdl' % args.outdir, 'w') as jdl:
-	jdl.write('''
+	jdl.write("""
 Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
 getenv = True
-executable = %s
+executable = %s/%s/batch_job.sh
 +MaxRuntime = 21600
-
-''' % spawn.find_executable('single_point_limit.py') )
+""" %  (proj_dir, args.outdir) )
 	idx = 0
 	for parity in args.parities.split(','):
 		for mass in masses:
 			for width in args.widths.split(','):
-				jdl.write('''
+				jdl.write("""
 Output = con_{idx}.out
 Error = con_{idx}.err
 Log = con_{idx}.log
 Arguments = {jobid} {parity} {mass} {width} {blind} {keep} {kfactor} {scan} {twoPars} {bb} {channels}
 Queue
-'''.format(
+""".format(
 				idx=idx,
 				jobid=jobid,
 				parity=parity,
